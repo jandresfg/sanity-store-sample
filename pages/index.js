c888db603831d144e86e8b9ff95b5a62e3503e8f
Andres Fernandez <andres.fernandez@truepill.om>
2022-06-17 03:28:29 -0500

    feat: Layout.js created and used inside index.js

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,5 @@
-import { Typography } from "@mui/material";
-import Head from "next/head";
+import Layout from "../components/Layout";
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Sanity Store</title>
-        <meta
-          name="description"
-          content="the eCommerce site by next and sanity"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Typography component={"h1"} variant="h1">
-        Sanity store
-      </Typography>
-    </div>
-  );
+  return <Layout>List products</Layout>;
 }
