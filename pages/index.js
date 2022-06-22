2b4ab703187eff286b4b61d74b70e05ce71bf858
Andres Fernandez <andres.fernandez@truepill.om>
2022-06-21 21:33:48 -0500

    feat: get product from sanity client and display products/loader/error

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,39 @@
+import { Alert, CircularProgress, Grid, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 import Layout from "../components/Layout";
+import client from "../utils/client";
 
 export default function Home() {
-  return <Layout>List products</Layout>;
+  const [state, setState] = useState({
+    products: [],
+    error: ",",
+    loading: true,
+  });
+  const { products, error, loading } = state;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const products = await client.fetch(`*[_type == "product"]`);
+        setState({ products, loading: false });
+      } catch (err) {
+        setState({ loading: false, error: err.message });
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <Layout>
+      {loading && <CircularProgress />}
+      {error && <Alert variant="danger">{error}</Alert>}
+      {!error && !loading && (
+        <Grid container spacing={3}>
+          {products.map((product) => (
+            <Grid item md={4} key={product.slug}>
+              {<Typography>{product.name}</Typography>}
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </Layout>
+  );
 }
